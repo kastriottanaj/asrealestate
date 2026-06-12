9816e33b929b8022679f6ec572f97e9cb7f8e41f
Kastriot <kastriot@Kastriots-MacBook-Air.local>
2026-06-12 10:08:49 +0200

    Make demo seeding opt-in during backend builds

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py seed_demo
+
+if [ "${SEED_DEMO:-False}" = "True" ]; then
+  python manage.py seed_demo
+fi
